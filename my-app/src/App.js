9cf4f0514727583aeef79f9d0a1f62bd28afc787
VeliKaracaaa <veli.karaca@epitech.eu>
2022-12-28 19:16:23 +0100

    rendu conditionnel de liste

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ import Button from './components/Button';
 const famille = {
   membre1: {
     name: 'Antho',
-    age: 7
+    age: 27
   },
   membre2: {
     name: 'Segolene',
@@ -14,7 +14,7 @@ const famille = {
   },
   membre3: {
     name: 'Eleanor',
-    age: 0
+    age: 18
   },
   membre4: {
     name: 'Mocha',
@@ -24,7 +24,8 @@ const famille = {
 
 class App extends Component {
   state = {
-    famille
+    famille,
+    isShow: false
   }
 
   handleClick = num => {
@@ -33,38 +34,59 @@ class App extends Component {
     this.setState({ famille })
   }
 
-  handleChange = event => {
+  handleChange = (event, id) => {
     const famille = { ...this.state.famille }
     const name = event.target.value
-    famille.membre1.name = name
+    famille[id].name = name
     console.log(name)
     this.setState({ famille })
   }
 
+  cacherName = id => {
+    const famille = { ...this.state.famille }
+    famille[id].name = 'X'
+    this.setState({ famille })
+  }
+
+  handleShowDescription = () => {
+    const isShow = !this.state.isShow
+    this.setState({ isShow })
+  }
+
   render () {
     const { titre } = this.props
-    const { famille } = this.state
+    const { famille, isShow } = this.state
+
+    let description = null
+
+    if (isShow) {
+      description = <strong>Je suis la</strong>
+    }
+
+    const liste = Object.keys(famille)
+      .map(membre => (
+        <Membre
+          key={membre}
+          handleChange={event => this.handleChange(event, membre)}
+          cacherName = {() => this.cacherName(membre)}
+          age={famille[membre].age}
+          name={famille[membre].name} />
+      ))
+    console.log(liste)
+
     return (
       <div>
         <div className='App'>
           <h1>{titre}</h1>
-          <input value={famille.membre1.name} onChange={this.handleChange} type='text' />
-          <Membre 
-            age={famille.membre1.age}
-            name={famille.membre1.name} />
-          <Membre 
-            name={famille.membre2.name} 
-            age={famille.membre2.age}/>
-          <Membre
-            name={famille.membre3.name} 
-            age={famille.membre3.age} />
-          <Membre 
+          { liste }
+          {/* <Membre 
             name={famille.membre4.name}
             age={famille.membre4.age}>
-            Je suis la
-          </Membre>
-          <Button 
-            vieillir={() => this.handleClick(2)} />
+            { description }
+            <button onClick={this.handleShowDescription}>Montrer</button>
+          </Membre> */}
+          {/* <Button 
+            vieillir={() => this.handleClick(2)} /> */}
         </div>
       </div>
     )
--- my-app/src/components/Membre.js
+++ my-app/src/components/Membre.js
@@ -1,6 +1,6 @@
 import React, { Children, Fragment } from "react";
 
-const Membre = ({ name, age, children, test }) => {
+const Membre = ({ name, age, children, cacherName, handleChange }) => {
     return (
         <Fragment>
             <h2 
@@ -8,6 +8,8 @@ const Membre = ({ name, age, children, test }) => {
                 color: age < 10 ? 'black' : 'white'}}>
                 {name} : {age}
             </h2>
+            <input value={name} onChange={handleChange} type='text' />
+            <button onClick={cacherName}>X</button>
             {children ? <p>{children}</p> : <Fragment />}
         </Fragment>
     )
